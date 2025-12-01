d997122592a34558c93f5eda3a65f3f3df12ab3e
Stepan <afanasevstepan67@gmail.com>
2025-12-01 14:04:24 +0400

    Initial commit

---
+++ src/main/java/ru/kursach/kpo/tour_agency_backend/TourAgencyBackendApplication.java
@@ -0,0 +1,13 @@
+package ru.kursach.kpo.tour_agency_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TourAgencyBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TourAgencyBackendApplication.class, args);
+	}
+
+}
